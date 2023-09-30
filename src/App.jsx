066d5a8351fe47f3f5765508b010be4ec49f8d32
Pritam Das <dpritam28577@gmail.com>
2023-09-30 16:14:18 +0530

    add the stytle in the tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import KanbanBoad from "./components/KanbanBoad";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      <KanbanBoad />
     </>
   );
 }
---
+++ src/components/KanbanBoad.jsx
@@ -0,0 +1,22 @@
+const KanbanBoad = () => {
+  return (
+    <div>
+      <button
+        className="
+        h-[60px]
+        w-[350px]
+        min-w-[350px]
+        cursor-pointer
+        rounded-lg
+        bg-blue-500
+        border-2
+        border-columnBackgroundColor     
+        "
+      >
+        Add coloumn
+      </button>
+    </div>
+  );
+};
+
+export default KanbanBoad;
