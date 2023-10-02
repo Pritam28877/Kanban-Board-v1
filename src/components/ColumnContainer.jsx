2ca38a3dbf22b43439aa8b5530bc5bd70f252379
Pritam Das <dpritam28577@gmail.com>
2023-10-02 19:32:14 +0530

    add the card styleing

--- src/components/ColumnContainer.jsx
+++ src/components/ColumnContainer.jsx
@@ -1,14 +1,41 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const ColumnContainer = (props) => {
-    const { column } = props;
-    return <div>{column.title}</div>;
+  const { column } = props;
+  return (
+    <div
+      className=" 
+     bg-columnBackgroundColor
+     flex
+     flex-col
+     w-[350px]
+     h-[500px]
+     min-w-[350px]
+     rounded-mg 
+     max-h-[500px]"
+    >
+      <div
+        className="bg-mainBackgroundColor
+      h-[60px]
+      text-md 
+      cursor-grab
+      rounded-md
+      rounded-b-none
+      p-3
+      font-bold
+      border-4
+      border-columnBackgroundColor"
+      >
+        {column.title}
+      </div>
+
+      <div>content</div>
+    </div>
+  );
 };
 
 ColumnContainer.propTypes = {
-    column: PropTypes.object.isRequired,
+  column: PropTypes.object.isRequired,
 };
 
 export default ColumnContainer;
-
-
