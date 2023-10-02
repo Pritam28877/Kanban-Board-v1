f2e95ba6a5e0de3de1dcb9e62ca1a07652954a19
Pritam Das <dpritam28577@gmail.com>
2023-10-02 22:37:09 +0530

    add the use memo hooks to track of the drag id

--- src/components/KanbanBoad.jsx
+++ src/components/KanbanBoad.jsx
@@ -1,12 +1,14 @@
 import PlusIcon from "../icons/PlusIcon";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import ColumnContainer from "./ColumnContainer";
-import { Dndcondex } from "react-dnd";
+import { DndContext } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
 
 const KanbanBoad = () => {
   const [columns, setColumns] = useState([]); // [ {id: 1, title: "To Do", cards: []}, {id: 2, title: "In Progress", cards: []}, {id: 3, title: "Done", cards: []}
 
-  console.log("columns", columns);
+  // console.log("columns", columns);
+  const columsId = useMemo(() => columns.map((col) => col.id), [columns]);
   const createNewColumn = () => {
     const newColumn = {
       id: columns.length + 1,
@@ -33,19 +35,22 @@ const KanbanBoad = () => {
       px-[40px]
     "
     >
-      <div className="m-auto flex gap-4">
-        <div className="flex gap-4 ">
-          {columns.map((column) => (
-            <ColumnContainer
-              key={column.id}
-              column={column}
-              deleteColumn={deleteColumn}
-            />
-          ))}
-        </div>
-        <button
-          onClick={() => createNewColumn()}
-          className="
+      <DndContext>
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4 ">
+            <SortableContext items={columsId}>
+              {columns.map((column) => (
+                <ColumnContainer
+                  key={column.id}
+                  column={column}
+                  deleteColumn={deleteColumn}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={() => createNewColumn()}
+            className="
         h-[60px]
         w-[350px]
         min-w-[350px]
@@ -58,11 +63,12 @@ const KanbanBoad = () => {
         gap-2
         border-red-600     
         "
-        >
-          <PlusIcon />
-          Add coloumn
-        </button>
-      </div>
+          >
+            <PlusIcon />
+            Add coloumn
+          </button>
+        </div>
+      </DndContext>
     </div>
   );
 };
